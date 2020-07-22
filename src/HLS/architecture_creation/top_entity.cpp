4757d376b357d4c2d0077562f940faeaec51d477
QiZhou1512 <qi1.zhou@mail.polimi.it>
2020-07-22 17:58:25 +0200

    connect output port

--- src/HLS/architecture_creation/top_entity.cpp
+++ src/HLS/architecture_creation/top_entity.cpp
@@ -194,9 +194,9 @@ DesignFlowStep_Status top_entity::InternalExec()
 
    /// connect to datapath and controller (gated) clock
    structural_objectRef datapath_clock = datapath_circuit->find_member(CLOCK_PORT_NAME, port_o_K, datapath_circuit);
-   SM->add_connection(datapath_clock, and_gate);
+   SM->add_connection(datapath_clock, GetPointer<module>(and_gate)->get_out_port(0)); 
    structural_objectRef controller_clock = controller_circuit->find_member(CLOCK_PORT_NAME, port_o_K, controller_circuit);
-   SM->add_connection(controller_clock, and_gate);
+   SM->add_connection(controller_clock, andGetPointer<module>(and_gate)->get_out_port(0));
    PRINT_DBG_MEX(DEBUG_LEVEL_VERY_PEDANTIC, debug_level, "\tClock signal added!");
 
    PRINT_DBG_MEX(DEBUG_LEVEL_VERY_PEDANTIC, debug_level, "\tAdding reset signal...");
