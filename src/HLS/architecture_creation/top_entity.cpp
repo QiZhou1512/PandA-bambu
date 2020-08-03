f50c1d77b39414619b0914308532490cf0a97838
QiZhou1512 <qi1.zhou@mail.polimi.it>
2020-08-03 16:41:09 +0200

    added second port

--- src/HLS/architecture_creation/top_entity.cpp
+++ src/HLS/architecture_creation/top_entity.cpp
@@ -184,15 +184,24 @@ DesignFlowStep_Status top_entity::InternalExec()
    structural_objectRef clock_gating_obj = SM->add_port(CLOCK_GATING_PORT_NAME, port_o::IN, circuit, bool_type);
 
    /// and gate for clock and clock gating
-   structural_objectRef andGateClockG = SM->add_module_from_technology_library("andGateClockG", AND_GATE_STD, LIBRARY_STD, circuit, HLS->HLS_T->get_technology_manager());
-   structural_objectRef port_objAndGate_in  = andGateClockG->find_member("in", port_vector_o_K, andGateClockG);
-   structural_objectRef port_objAndGate_out1 = andGateClockG->find_member("out1", port_o_K, andGateClockG);
-   structural_objectRef port_objAndGate_out2 = andGateClockG->find_member("out2", port_o_K, andGateClockG);
-   auto* in_portAndGate = GetPointer<port_o>(port_objAndGate_in);
-   in_portAndGate->add_n_ports(2, port_objAndGate_in);
+   //first AND gate
+   structural_objectRef andGateClockG1 = SM->add_module_from_technology_library("andGateClockG", AND_GATE_STD, LIBRARY_STD, circuit, HLS->HLS_T->get_technology_manager());
+   structural_objectRef port_objAndGate_in1  = andGateClockG->find_member("in", port_vector_o_K, andGateClockG1);
+   structural_objectRef port_objAndGate_out1 = andGateClockG->find_member("out1", port_o_K, andGateClockG1);
+   auto* in_portAndGate1 = GetPointer<port_o>(port_objAndGate_in1);
+   in_portAndGate1->add_n_ports(2, port_objAndGate_in1);
 
-   SM->add_connection(clock_obj, in_portAndGate->get_port(0));
-   SM->add_connection(clock_gating_obj, in_portAndGate->get_port(1));
+   SM->add_connection(clock_obj, in_portAndGate1->get_port(0));
+   SM->add_connection(clock_gating_obj, in_portAndGate1->get_port(1));
+   //second AND gate
+   structural_objectRef andGateClockG2 = SM->add_module_from_technology_library("andGateClockG", AND_GATE_STD, LIBRARY_STD, circuit, HLS->HLS_T->get_technology_manager());
+   structural_objectRef port_objAndGate_in2  = andGateClockG->find_member("in", port_vector_o_K, andGateClockG2);
+   structural_objectRef port_objAndGate_out2 = andGateClockG->find_member("out1", port_o_K, andGateClockG2);
+   auto* in_portAndGate2 = GetPointer<port_o>(port_objAndGate_in2);
+   in_portAndGate2->add_n_ports(2, port_objAndGate_in2);
+
+   SM->add_connection(clock_obj, in_portAndGate2->get_port(0));
+   SM->add_connection(clock_gating_obj, in_portAndGate2->get_port(1));
 
    /// connect to datapath and controller (gated) clock
    structural_objectRef datapath_clock = datapath_circuit->find_member(CLOCK_PORT_NAME, port_o_K, datapath_circuit);
