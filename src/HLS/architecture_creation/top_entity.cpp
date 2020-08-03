c4379954f3d768030ae6c36f85be0bf66df8ead5
QiZhou1512 <qi1.zhou@mail.polimi.it>
2020-08-03 16:00:12 +0200

    minor fix

--- src/HLS/architecture_creation/top_entity.cpp
+++ src/HLS/architecture_creation/top_entity.cpp
@@ -184,20 +184,22 @@ DesignFlowStep_Status top_entity::InternalExec()
    structural_objectRef clock_gating_obj = SM->add_port(CLOCK_GATING_PORT_NAME, port_o::IN, circuit, bool_type);
 
    /// and gate for clock and clock gating
-   structural_objectRef andGateClockG = SM->add_module_from_technology_library("andGateClockG", AND_GATE_STD, HLS->HLS_T->get_technology_manager()->get_library(AND_GATE_STD), circuit, HLS->HLS_T->get_technology_manager());
+   structural_objectRef andGateClockG = SM->add_module_from_technology_library("andGateClockG", AND_GATE_STD, LIBRARY_STD, circuit, HLS->HLS_T->get_technology_manager());
    structural_objectRef port_objAndGate_in  = andGateClockG->find_member("in", port_vector_o_K, andGateClockG);
    structural_objectRef port_objAndGate_out = andGateClockG->find_member("out1", port_o_K, andGateClockG);
    auto* in_portAndGate = GetPointer<port_o>(port_objAndGate_in);
+   auto* out_portAndGate = GetPointer<port_o>(port_objAndGate_out);
    in_portAndGate->add_n_ports(2, port_objAndGate_in);
+   out_portAndGate->add_n_ports(2, port_objAndGate_out);
 
    SM->add_connection(clock_obj, in_portAndGate->get_port(0));
    SM->add_connection(clock_gating_obj, in_portAndGate->get_port(1));
 
    /// connect to datapath and controller (gated) clock
    structural_objectRef datapath_clock = datapath_circuit->find_member(CLOCK_PORT_NAME, port_o_K, datapath_circuit);
-   SM->add_connection(port_objAndGate_out, datapath_clock);
+   SM->add_connection(out_portAndGate->get_port(0), datapath_clock);
    structural_objectRef controller_clock = controller_circuit->find_member(CLOCK_PORT_NAME, port_o_K, controller_circuit);
-   SM->add_connection(port_objAndGate_out, controller_clock);
+   SM->add_connection(out_portAndGate->get_port(1), controller_clock);
    PRINT_DBG_MEX(DEBUG_LEVEL_VERY_PEDANTIC, debug_level, "\tClock signal added!");
 
    PRINT_DBG_MEX(DEBUG_LEVEL_VERY_PEDANTIC, debug_level, "\tAdding reset signal...");
